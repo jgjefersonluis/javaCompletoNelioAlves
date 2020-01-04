1d102a21a2c8e0768133180df658968556f3f364
Jeferson Luis <jgjefersonluis@gmail.com>
2020-01-04 01:27:24 -0300

    seçao 1 a 4 completa + exercicios

---
+++ curso_programacao/src/MainScannerDouble.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MainScannerDouble {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double x;
+		x = sc.nextDouble();
+		System.out.printf ("Você digitou: %.2f%n", x);
+		
+		sc.close();
+
+	}
+
+}
---
+++ curso_programacao/src/MainScannerInt.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MainScannerInt {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		x = sc.nextInt();
+		System.out.println("Você digitou: " + x);
+		
+		sc.close();
+
+	}
+
+}
